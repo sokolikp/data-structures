9fe0ae16fde0a6bf49f442c417910cf83121eda1
Hack Reactor Students <accounts@hackreactor.com>
2015-05-13 15:34:28 -0700

    Finished functional shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,31 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {front: 0,
+             end: 0,
+             storage: {}};
+  for (var key in queueMethods) {
+    obj[key] = queueMethods[key];
+  }
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.end] = value;
+    this.end++;
+  },
 
+  dequeue : function(){
+    if(this.front < this.end) {
+      var temp = this.storage[this.front];
+      this.front++;
+      return temp;
+    }
+  },
 
+  size : function(){
+    return this.end - this.front;
+  }
 
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {objSize: 0,
+             storage: {}};
+  for (var key in stackMethods) {
+    obj[key] = stackMethods[key];
+  }
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value){
+    this.storage[this.objSize] = value;
+    this.objSize++;
+  },
+
+  pop : function(){
+    if (this.objSize > 0) {
+      this.objSize--;
+      var temp = this.storage[this.objSize];
+      delete this.storage[this.objSize];
+      return temp;
+    }
+  },
+
+  size : function(){
+    return this.objSize;
+  }
+};
 
 
