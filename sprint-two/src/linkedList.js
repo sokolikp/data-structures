f6966ca729a23f31a86fe4a3ffadffa86cc80ad2
Hack Reactor Students <accounts@hackreactor.com>
2015-05-14 10:40:29 -0700

    completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,34 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
-
   list.addToTail = function(value){
+    if(list.tail !== null) {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
+    else {
+      list.head = Node(value);
+      list.tail = list.head;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head !== null) {
+      var result = list.head.value;
+      list.head = list.head.next;
+      return result;
+    }
   };
 
   list.contains = function(target){
+    var currentObj = list.head;
+    while(currentObj !== null) {
+      if(currentObj.value === target) {
+          return true;
+      }
+      currentObj = currentObj.next;
+    }
+    return false;
   };
 
   return list;
