0cfbddb2257204dae59760d82649d4cc36a5ae9e
Hack Reactor Students <accounts@hackreactor.com>
2015-05-15 14:29:36 -0700

    finished has, attempted tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,47 @@
 var BinarySearchTree = function(value){
+  var newTree = {};
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  _.extend(newTree, bstMethods);
+  return newTree;
 };
 
+var bstMethods = {};
+
+bstMethods.insert = function(value) {
+  //var branch = BinarySearchTree(val);
+  if (this.value < value) {
+    if (this.left.value === null) {
+      this.left.value = BinarySearchTree(value);
+    }
+    else {
+      this.left.insert(value);
+    }
+  }
+  else {
+    if (this.right.value === null) {
+      this.right.value = BinarySearchTree(value);
+    }
+    else {
+      this.right.insert(value);
+    }
+  }
+};
+
+bstMethods.contains = function(value) {
+
+};
+
+bstMethods.depthFirstLog = function(value) {
+
+};
+
+//var Node = function (value) {
+//  return {value: value, right: null, left: null};
+
+//var tree = new BinarySearchTree(1);
+//console.log(tree);
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,34 +3,63 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+/*HashTable.prototype.checkSize = function() {
+  var count = 0;
+  var max = 0;
+  for (var i = 0; i < this._limit; i++) {
+    var ref = this._storage.get(i);
+    if(ref !== undefined) {
+      for (var j = 0 ; j < ref.length; j++) {
+        if (ref[j].length === 2) {
+          count++;
+        }
+      }
+      if(count > max) {
+        max = count;
+      }
+    }
+  }
+  if (max/this._limit > 0.75) {
+    this._limit *= 2;
+  }
+};*/
+
 HashTable.prototype.insert = function(k, v){
+  //this.checkSize();
   var i = getIndexBelowMaxForKey(k, this._limit);
   var ref = this._storage.get(i);
   if(ref === undefined) {
-    var insert = {};
-    insert[k] = v;
+    var insert = [];
+    insert.push([k, v]);
     this._storage.set(i, insert);
   }
   else {
-    ref[k] = v;
+    ref.push([k,v]);
   }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i)[k] || null;
-  //return ref[k];
+  var ref = this._storage.get(i);
+  for (var j = 0; j < ref.length; j++) {
+    if (ref[j][0] === k) {
+      return ref[j][1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var ref = this._storage.get(i);
-  delete ref[k];
-  //this._storage.set(i, null);
+  for (var j = 0; j < ref.length; j++) {
+    if (ref[j][0] === k) {
+      ref[j] =  [];
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-//obj = {a: 'b'};
-//delete obj[a];
+
