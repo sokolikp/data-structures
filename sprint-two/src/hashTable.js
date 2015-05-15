10119365c4d8d9e995362cca8b5c1ff2c60b82ef
Hack Reactor Students <accounts@hackreactor.com>
2015-05-14 20:38:48 -0700

    completed hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,19 +5,32 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var ref = this._storage.get(i);
+  if(ref === undefined) {
+    var insert = {};
+    insert[k] = v;
+    this._storage.set(i, insert);
+  }
+  else {
+    ref[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage.get(i)[k] || null;
+  //return ref[k];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var ref = this._storage.get(i);
+  delete ref[k];
+  //this._storage.set(i, null);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//obj = {a: 'b'};
+//delete obj[a];
