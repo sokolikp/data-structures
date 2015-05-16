cc60e55d094faa973d6a7038bde169b5efcb1df9
Hack Reactor Students <accounts@hackreactor.com>
2015-05-16 12:01:13 -0700

    Started hashtable resize

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,31 +1,45 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._numRecords = 0;
 };
 
-/*HashTable.prototype.checkSize = function() {
-  var count = 0;
-  var max = 0;
-  for (var i = 0; i < this._limit; i++) {
-    var ref = this._storage.get(i);
-    if(ref !== undefined) {
-      for (var j = 0 ; j < ref.length; j++) {
-        if (ref[j].length === 2) {
-          count++;
+HashTable.prototype.checkSize = function() {
+  if(this._numRecords/this._limit >= 0.75) {
+    var oldStorage = this._storage;
+    this._limit *= 2;
+    this._storage = LimitedArray(this._limit);
+    var context = this;
+    oldStorage.each(function(house) {
+      if(house !== undefined) {
+        for(var i=0; i<house.length; i++) {
+          if(house[i][0] !== undefined) {
+            context.insert(house[i][0], house[i][1]);
+          }
         }
       }
-      if(count > max) {
-        max = count;
+    });
+  }
+  else if(this._numRecords/this._limit <= 0.25) {
+    var oldStorage = this._storage;
+    this._limit = Math.ceil(this._limit/2);
+    this._storage = LimitedArray(this._limit);
+    var context = this;
+    oldStorage.each(function(house) {
+      if(house !== undefined) {
+        for(var i=0; i<house.length; i++) {
+          if(house[i][0] !== undefined) {
+            context.insert(house[i][0], house[i][1]);
+          }
+        }
       }
-    }
+    });
   }
-  if (max/this._limit > 0.75) {
-    this._limit *= 2;
-  }
-};*/
+};
 
 HashTable.prototype.insert = function(k, v){
-  //this.checkSize();
+  this._numRecords++;
+  this.checkSize();
   var i = getIndexBelowMaxForKey(k, this._limit);
   var ref = this._storage.get(i);
   if(ref === undefined) {
@@ -50,11 +64,14 @@ HashTable.prototype.retrieve = function(k){
 };
 
 HashTable.prototype.remove = function(k){
+  this.numRecords--;
+  this.checkSize();
   var i = getIndexBelowMaxForKey(k, this._limit);
   var ref = this._storage.get(i);
   for (var j = 0; j < ref.length; j++) {
     if (ref[j][0] === k) {
       ref[j] =  [];
+      //this._numRecords--;
     }
   }
 };
